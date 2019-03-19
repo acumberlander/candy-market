3b87d18d6b2fec594a622192f130101ae9f0e310
WaysideTester <dylanmurray211@outlook.com>
2019-03-18 20:11:50 -0500

    uncommenting constructor

--- Candy.cs
+++ Candy.cs
@@ -4,17 +4,17 @@ namespace candy_market
 {
     internal class Candy
     {
-        public string Name { get; }
+        public string Name { get; set; }
         public string Manufacturer { get; }
         public string FlavorCategory { get; }
         public DateTime DateRecieved { get; }
 
-        public Candy(string name, string manufacturer, string falvorCategory)
+      /*  public Candy(string name, string manufacturer, string falvorCategory)
         {
             Name = name;
             Manufacturer = manufacturer;
             FlavorCategory = FlavorCategory;
             DateRecieved = DateTime.Now;
-        }
+        } */
     }
 }
